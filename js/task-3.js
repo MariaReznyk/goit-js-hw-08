f1ce67e5c63c42b0b98d36b43ee07cae64f4a712
maria_reznyk <maria.reznyk@gmail.com>
2025-01-16 00:06:17 +0000

    homework done

--- js/task-3.js
+++
@@ -1,9 +0,0 @@
-const nameInput = document.querySelector('#name-input');
-const nameOutput = document.querySelector('#name-output');
-
-nameInput.addEventListener('input', () => {
-    const nameInputEdited = nameInput.value.trim();
-    nameOutput.textContent = nameInputEdited;
-
-    if (nameInputEdited === '') {nameOutput.textContent = 'Anonymous'}
-})
